716b63c6648762a5b875336e7e03d65f1b681f11
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-05 20:01:14 +0000

    going on

--- answers.js
+++ answers.js
@@ -78,3 +78,64 @@ function string_num(strA,numG) {
 }
 
 string_num("hi",9)
+
+
+
+
+function reverseStr(strB) {
+    
+    strFinal = "" ;
+    for (i = strB.length ; i >= 0 ; i--) {
+        strFinal = strFinal + strB.charAt(i) ;
+        
+    }
+    
+    return strFinal ;
+}
+reverseStr("shreyakaicker") ;
+
+
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+    
+  return n * factorial(n - 1);
+}
+console.log(factorial(5));
+
+/*
+recursion starts from bottom
+
+ 5 * factorial(5-1); 5 * 24 = 120
+ 4 * factorial(4-1); 4 * 6 = 24
+ 3 * factorial(3-1); 3 * 2 = 6
+ 2 * factorial(2-1); 2 * 1 = 2
+ 1 * factorial(1-1); 1 * 1 = 1
+
+*/ 
+ 
+ 
+
+function longestWord(string1) {
+    var str = string1.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length ; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
+
+longestWord("My name is ShreyaKaicker");
+
+
+
+
+
+
+
