44db1a479e2aee2f8c71f1391b61fbc0520c5404
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-05 18:01:24 +0000

    second ans

--- answers.js
+++ answers.js
@@ -1,1 +1,13 @@
 // this is a comment
+
+function stringFirstChar(sentence) {
+    return sentence.substring(0,1)
+}
+stringFirstChar("hello");
+
+
+function stringLastChar(sentence1) {
+    var stringLength = sentence1.length ;
+    return sentence1.substring(stringLength,stringLength-1)
+}
+stringLastChar("hello");
