0a35061b725a066b97284e8d95da43a6d1ee97b2
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-05 18:24:41 +0000

    third commit

--- answers.js
+++ answers.js
@@ -6,8 +6,18 @@ function stringFirstChar(sentence) {
 stringFirstChar("hello");
 
 
+
+
 function stringLastChar(sentence1) {
     var stringLength = sentence1.length ;
     return sentence1.substring(stringLength,stringLength-1)
 }
 stringLastChar("hello");
+
+
+
+
+function strAndNum(str1,num1) {
+    return str1.charAt(num1);
+}
+strAndNum("hello",2);
